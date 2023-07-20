900e92a4ca0c0d5ec7c74ae83f9440e577da3621
furukawaeiichi <102509805+furukawaeiichi@users.noreply.github.com>
2023-07-20 22:20:55 +0900

    gem rails-i18nの設定をしました

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module CREDOQuest
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.default_locale = :ja
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
