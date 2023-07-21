22b4bac5718dc304d721e94db261a6ab45a9eeeb
furukawaeiichi <102509805+furukawaeiichi@users.noreply.github.com>
2023-07-21 18:29:28 +0900

    jpegをjpgに修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,13 +15,13 @@ module ApplicationHelper
         description: :description,
         type: 'website',
         url: request.original_url,
-        image: image_url('ogp.jpeg'),
+        image: image_url('ogp.jpg'),
         local: 'ja-JP'
       },
       twitter: {
         card: 'summary_large_image',
         site: '@CREDO_QUEST',
-        image: image_url('ogp.jpeg')
+        image: image_url('ogp.jpg')
       }
     }
   end
