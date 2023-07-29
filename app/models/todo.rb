2584cb8d55f5682de3624c80f33fcf989c69a454
furukawaeiichi <102509805+furukawaeiichi@users.noreply.github.com>
2023-07-29 23:27:31 +0900

    ユーザーのtodo check数をLevelとして表示

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,16 @@
 class Todo < ApplicationRecord
   belongs_to :user
+  after_save :update_user_level, if: :saved_change_to_checked?
 
   scope :default_todos, -> { where(default: true) }
   scope :user_todos, -> { where(default: false) }
+
+  def update_user_level
+    if self.checked
+      self.user.level += 1
+    else
+      self.user.level -= 1
+    end
+    self.user.save
+  end
 end
