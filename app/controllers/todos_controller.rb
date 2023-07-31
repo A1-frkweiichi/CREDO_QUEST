72f905d257a8f20e01bc11a574675e395abb824b
furukawaeiichi <102509805+furukawaeiichi@users.noreply.github.com>
2023-07-31 18:35:23 +0900

    flash message

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -5,17 +5,19 @@ class TodosController < ApplicationController
     if @todo.update(todo_params)
       @todo.update_user_level
       respond_to do |format|
-        format.turbo_stream
+        format.turbo_stream do
+          flash.now[:notice] = t('flash.todos.update.success')
+        end
         format.html { redirect_to request.referer || root_path, notice: 'Todo was successfully updated.' }
       end
     else
       respond_to do |format|
         format.turbo_stream do
-          flash.now[:error] = 'Todo update failed.'
+          flash.now[:error] = t('flash.todos.update.failure')
           render turbo_stream: turbo_stream.replace('user_level', partial: 'users/level', locals: { user: current_user })
         end
         format.html do
-          flash[:error] = 'Todo update failed.'
+          flash[:error] = t('flash.todos.update.failure')
           redirect_to request.referer || root_path
         end
       end
