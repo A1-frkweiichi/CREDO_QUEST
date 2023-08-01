94619eb19918b777f49fb22eceaca1813ccd7a44
furukawaeiichi <102509805+furukawaeiichi@users.noreply.github.com>
2023-08-01 23:16:51 +0900

    progress-bar テスト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  include UsersHelper
 
   def index
     @users = User.order(last_login_at: :desc).page(params[:page]).per(10)
@@ -8,6 +9,7 @@ class UsersController < ApplicationController
   def show
     @default_todos_grouped = @user.todos.default_todos.order(id: :asc).group_by(&:category)
     # @user_todos_grouped = @user.todos.user_todos.order(id: :desc).group_by(&:category)
+    @completion_percentage = default_todos_completion_percentage
   end
 
   def edit
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+  def default_todos_completion_percentage
+    total_default_todos = 50
+    completed_default_todos = current_user.todos.default_todos.where(checked: true).count
+
+    (completed_default_todos.to_f / total_default_todos.to_f) * 100
+  end
 end
