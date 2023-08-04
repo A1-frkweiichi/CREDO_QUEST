3abb16d5acec601f10f9f3a2ef310c4b583cd860
furukawaeiichi <102509805+furukawaeiichi@users.noreply.github.com>
2023-08-04 18:38:32 +0900

    rubocop修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,14 +19,14 @@ class User < ApplicationRecord
     user = self.find_or_create_by(uid: auth[:uid], provider: auth[:provider]) do |user_record|
       user_record.nickname = auth[:info][:nickname]
       user_record.name = auth[:info][:name]
-      user_record.image = auth[:info][:image].gsub('_normal', '')
+      user_record.image = auth[:info][:image].gsub("_normal", "")
       user_record.last_login_at = Time.current
     end
 
     user.assign_attributes(
       nickname: auth[:info][:nickname],
       name: auth[:info][:name],
-      image: auth[:info][:image].gsub('_normal', ''),
+      image: auth[:info][:image].gsub("_normal", ""),
       last_login_at: Time.current
     )
 
