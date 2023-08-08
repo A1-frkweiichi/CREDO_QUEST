d99575cd3eb6488db98c1acf4444bb8a86b00a0d
furukawaeiichi <102509805+furukawaeiichi@users.noreply.github.com>
2023-08-09 03:06:45 +0900

    TODO CRUD機能

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,13 +1,45 @@
 class TodosController < ApplicationController
   before_action :set_todo, only: [:show, :edit, :update, :destroy]
 
+  def index
+    @todos = Todo.user_todos
+  end
+
+  def show
+  end
+
+  def new
+    @todo = current_user.todos.new(default: false)
+  end
+
+  def edit
+  end
+
+  def create
+    @todo = current_user.todos.new(todo_params.merge(default: false))
+
+    if @todo.save
+      redirect_to todo_url(@todo), notice: "Todo was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def update
     if @todo.update(todo_params)
-      @todo.update_user_level
-      @categories_progress = @todo.user.categories_progress
+      update_user_level_and_categories_progress
       flash.now[:notice] = t("flash.todos.update.success")
     else
       flash.now[:error] = t("flash.todos.update.failure")
+    end
+  end
+
+  def destroy
+    if @todo.default == false
+      @todo.destroy
+      redirect_to todos_url, notice: "Todo was successfully destroyed."
+    else
+      redirect_to todos_url, alert: "Not allowed to delete default todos."
     end
   end
 
@@ -18,6 +50,11 @@ class TodosController < ApplicationController
   end
 
   def todo_params
-    params.require(:todo).permit(:checked)
+    params.require(:todo).permit(:category, :description, :checked)
+  end
+
+  def update_user_level_and_categories_progress
+    @todo.update_user_level
+    @categories_progress = @todo.user.categories_progress
   end
 end
