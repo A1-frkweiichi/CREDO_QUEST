17d77645f5cfd266c6a190c526eeb82eb2d4b836
furukawaeiichi <102509805+furukawaeiichi@users.noreply.github.com>
2023-08-11 15:12:01 +0900

    todo Twitterシェア デバッグ

--- app/helpers/todos_helper.rb
+++ app/helpers/todos_helper.rb
@@ -1,12 +1,2 @@
 module TodosHelper
-  def todo_meta_tags
-    set_meta_tags(
-      og: {
-        image: ActionController::Base.helpers.image_url("ogp.jpg")
-      },
-      twitter: {
-        image: ActionController::Base.helpers.image_url("ogp.jpg")
-      }
-    )
-  end
 end
