905843f0e8de38673f8c5a17181226b140716e24
furukawaeiichi <102509805+furukawaeiichi@users.noreply.github.com>
2023-08-11 21:27:02 +0900

    todo Twitterシェア デバッグ

--- app/helpers/todos_helper.rb
+++ app/helpers/todos_helper.rb
@@ -2,6 +2,7 @@ module TodosHelper
   def todo_meta_tags
     set_meta_tags(
       og: {
+        url: "https://www.credo-quest.com/todos",
         image: ActionController::Base.helpers.image_url("ogp.jpg")
       },
       twitter: {
