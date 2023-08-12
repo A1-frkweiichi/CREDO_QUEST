83b1140f5b8ddee2944a6f23be03aa3691c84188
furukawaeiichi <102509805+furukawaeiichi@users.noreply.github.com>
2023-08-12 22:24:56 +0900

    current_user nil 条件分岐

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,19 @@
 class LikesController < ApplicationController
+  before_action :set_todo
+
   def create
-    @todo = Todo.find(params[:todo_id])
     current_user.like(@todo)
     turbo_stream
   end
 
   def destroy
-    @todo = Todo.find(params[:todo_id])
     current_user.unlike(@todo)
     turbo_stream
+  end
+
+  private
+
+  def set_todo
+    @todo = Todo.find(params[:todo_id])
   end
 end
